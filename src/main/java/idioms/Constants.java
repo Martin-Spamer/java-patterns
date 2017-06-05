ac86d1120d6f7fe0195c6479ee858e19e214a61b
Martin Spamer <github@atf4j.com>
2017-06-05 19:52:19 +0100

    Public static constants.

--- src/main/java/idioms/Constants.java
+++ src/main/java/idioms/Constants.java
@@ -7,28 +7,29 @@
 package idioms;
 
 /**
- * The Constants Class.
+ * Constants Class.
  */
 public class Constants {
 
-	/** The Constant COUNTRY_CODE. */
+	/** COUNTRY_CODE Constant. */
 	public static final String COUNTRY_CODE = "uk";
 
-	/** The Constant COUNTRY_NAME. */
+	/** COUNTRY_NAME Constant. */
 	public static final String COUNTRY_NAME = "United Kingdom";
 
-	/** The Constant LANGUAGE_CODE. */
+	/** LANGUAGE_CODE Constant. */
 	public static final String LANGUAGE_CODE = "en";
 
-	/** The Constant LANGUAGE_NAME. */
+	/** LANGUAGE_NAME Constant. */
 	public static final String LANGUAGE_NAME = "English";
 
-	/** The Constant CURRENCY_CODE. */
+	/** CURRENCY_CODE Constant. */
 	public static final String CURRENCY_CODE = "GPB";
 
-	/** The Constant CURRENCY_GLYPH. */
+	/** CURRENCY_GLYPH Constant. */
 	public static final String CURRENCY_GLYPH = "&#163";
 
-	/** The Constant CURRENCY_NAME. */
+	/** CURRENCY_NAME Constant. */
 	public static final String CURRENCY_NAME = "pound";
+
 }
