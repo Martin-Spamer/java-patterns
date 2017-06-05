82d7ab50f7dc1e3741c18107110fccebc1b998f2
Martin Spamer <github@atf4j.com>
2017-06-05 19:51:48 +0100

    Make class runnable from command line.

--- src/main/java/idioms/ClassMain.java
+++ src/main/java/idioms/ClassMain.java
@@ -1,9 +1,21 @@
-package javamentor;
 
+package idioms;
+
+/**
+ * Example of Main entry point when run from command line.
+ */
 public class ClassMain {
+
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args) {
 		for (final String string : args) {
 			System.out.println(string);
 		}
+
+		System.out.println("%s = " + Constants.COUNTRY_NAME);
 	}
 }
