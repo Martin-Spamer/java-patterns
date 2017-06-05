c3edac8cad9283628fc8f3f2e4db99ed4bfba0f0
Martin Spamer <github@atf4j.com>
2017-06-05 19:53:36 +0100

    Simple factory method example

---
+++ src/main/java/idioms/FactoryMethod.java
@@ -0,0 +1,19 @@
+
+package idioms;
+
+/**
+ * Factory Method Example.
+ */
+public class FactoryMethod {
+
+	/**
+	 * Creates instance of class from fully qualified name.
+	 *
+	 * @return the string
+	 * @throws Exception the exception
+	 */
+	public static String create() throws Exception {
+		return (String) Class.forName("java.lang.String").newInstance();
+	}
+
+}
