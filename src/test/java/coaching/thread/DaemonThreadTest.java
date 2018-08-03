69388d6aae90e2f4f2d8519f64dc26248bffc519
Martin Spamer <martin.spamer@gmail.com>
2018-08-03 13:15:25 +0100

    Remove some old useless examples.

---
+++ src/test/java/coaching/thread/DaemonThreadTest.java
@@ -0,0 +1,27 @@
+
+package coaching.thread;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.Assert.assertNotNull;
+
+public class DaemonThreadTest {
+
+    /** provides logging */
+    private static final Logger LOG = LoggerFactory
+        .getLogger(DaemonThreadTest.class);
+
+    @Test
+    public void testRunDaemonThread() {
+        DaemonThread daemon = DaemonThread.getSingleton();
+        assertNotNull(daemon);
+        LOG.info("{}", daemon);
+        daemon.start();
+        LOG.info("{}", daemon);
+        daemon.interrupt();
+        LOG.info("{}", daemon);
+    }
+
+}
